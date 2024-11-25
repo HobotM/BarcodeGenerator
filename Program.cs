4eb16aacefb8160db90ee70f49db82ac80e16dea
mateusz <mateuszhobot@gmail.com>
2024-11-25 15:42:56 +0000

    Initial commit - Added barcode generator project with README and gitignore

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing;
+using ZXing.Common;
+
+namespace BarcodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the text to encode: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Enter the filename (without extension): ");
+            string filename = Console.ReadLine() + ".png";
+
+            var barcodeWriter = new BarcodeWriterPixelData
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions
+                {
+                    Height = 100,
+                    Width = 300,
+                    Margin = 10
+                }
+            };
+
+            var pixelData = barcodeWriter.Write(text);
+
+            using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb))
+            {
+                var bitmapData = bitmap.LockBits(
+                    new Rectangle(0, 0, pixelData.Width, pixelData.Height),
+                    ImageLockMode.WriteOnly,
+                    PixelFormat.Format32bppRgb);
+                try
+                {
+                    System.Runtime.InteropServices.Marshal.Copy(
+                        pixelData.Pixels,
+                        0,
+                        bitmapData.Scan0,
+                        pixelData.Pixels.Length);
+                }
+                finally
+                {
+                    bitmap.UnlockBits(bitmapData);
+                }
+
+                bitmap.Save(filename, ImageFormat.Png);
+                Console.WriteLine($"Barcode saved as {filename}");
+            }
+        }
+    }
+}
